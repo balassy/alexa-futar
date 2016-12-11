d768003e602d341168ebfaca42eebb3915ba79c8
György Balássy <balassy@users.noreply.github.com>
2016-12-11 22:10:49 +0100

    REFACTORED: Moved webservice call to the service class.

--- direct/lambda-src/futar-service.js
+++ direct/lambda-src/futar-service.js
@@ -2,12 +2,51 @@
 
 const moment = require('moment');
 const momentTimezone = require('moment-timezone');
+const rp = require('request-promise-lite');
 
 const TIMEZONE_NAME = 'Europe/Budapest';
+const MINUTES_AFTER = 90;
 
 module.exports = class FutarService {
 
-  getNextRidesInLocalTime(currentTimeInMilliseconds, firstRide, secondRide) {
+  getNextRides(stopId) {
+    const url = `http://futar.bkk.hu/bkk-utvonaltervezo-api/ws/otp/api/where/arrivals-and-departures-for-stop.json?stopId=${stopId}&onlyDepartures=true&minutesBefore=0&minutesAfter=${MINUTES_AFTER}`;
+
+    const options = {
+      resolveWithFullResponse: true,
+      json: true
+    };
+
+    return rp.get(url, options)
+      .then(response => {
+        if (response.statusCode !== 200) {
+          throw new Error('Sorry, your webservice call failed with HTTP ${response.statusCode} status code! The server returned: ${JSON.stringify(response)}');
+        }
+
+        const body = response.body;
+
+        if (body.version !== 2) {
+          throw new Error(`The Futár webservice returned a response body with a version different than 2: ${response.version}`);
+        }
+
+        if (body.status !== 'OK') {
+          throw new Error(`The Futár webservice returned a response body with a status different than OK: ${response.status}`);
+        }
+
+        if (body.code !== 200) {
+          throw new Error(`The Futár webservice returned a response body with a code different than 200: ${response.code}`);
+        }
+
+        const currentTimeInMilliseconds = body.currentTime;
+        const firstRide = body.data.entry.stopTimes[0];
+        const secondRide = body.data.entry.stopTimes[1];
+
+        const rideTimes = this._getNextRidesInLocalTime(currentTimeInMilliseconds, firstRide, secondRide);
+        return rideTimes;
+      })
+  }
+
+  _getNextRidesInLocalTime(currentTimeInMilliseconds, firstRide, secondRide) {
     const currentTimeInLocalTime = this._getTimeInLocalTime(currentTimeInMilliseconds);
     const firstRideTimeInLocalTime = this._getRideTimeInLocalTime(firstRide);
     const secondRideTimeInLocalTime = this._getRideTimeInLocalTime(secondRide);
@@ -29,7 +68,7 @@ module.exports = class FutarService {
   _getRideTimeInLocalTime(ride) {
     const rideTimeInMilliseconds = (ride.predictedArrivalTime || ride.arrivalTime || ride.departureTime) * 1000;
     const rideTimeInLocalTime = this._getTimeInLocalTime(rideTimeInMilliseconds);
-    return rideTimeInLocalTime; 
+    return rideTimeInLocalTime;
   }
 
   _getTimeInLocalTime(timeInMilliseconds) {
--- direct/lambda-src/index.js
+++ direct/lambda-src/index.js
@@ -1,13 +1,10 @@
 'use strict';
 
-const rp = require('request-promise-lite');
 const Alexa = require('alexa-sdk');
 const FutarService = require('./futar-service');
 
 const SKILL_NAME = 'Next Ride';
 const TRAM_STOP_ID = 'BKK_F02296';
-const MINUTES_AFTER = 90;
-const SERVICE_URL = `http://futar.bkk.hu/bkk-utvonaltervezo-api/ws/otp/api/where/arrivals-and-departures-for-stop.json?stopId=${TRAM_STOP_ID}&onlyDepartures=true&minutesBefore=0&minutesAfter=${MINUTES_AFTER}`;
 
 const handlers = {
   'LaunchRequest': function () {
@@ -17,23 +14,28 @@ const handlers = {
     this.emit('GetNextRide');
   },
   'GetNextRide': function () {
-    const options = {
-      resolveWithFullResponse: true,
-      json: true
-    };
+    const futarService = new FutarService();
+    futarService.getNextRides(TRAM_STOP_ID)
+      .then(rides => {
+        let speechOutput;
 
-    rp.get(SERVICE_URL, options)
-      .then((response) => {
-        if (response.statusCode !== 200) {
-          const details = 'Sorry, your webservice call failed with HTTP ${response.statusCode} status code! The server returned: ${JSON.stringify(response)}';
-          _emitFailure(details);
-          return;
+        if (rides.firstRideRelativeTimeInMinutes >= 8) {
+          speechOutput = `Your next tram goes in ${rides.firstRideRelativeTimeHumanized} at ${rides.firstRideAbsoluteTime}.  You can easily catch it. The second tram goes ${rides.secondRideRelativeTimeHumanized} later at ${rides.secondRideAbsoluteTime}.`;
+        }
+        else if (rides.firstRideRelativeTimeInMinutes < 8 && rides.firstRideRelativeTimeInMinutes >= 5) {
+          speechOutput = `Your next tram goes in ${rides.firstRideRelativeTimeHumanized} at ${rides.firstRideAbsoluteTime}.  You can still catch it. After that you have to wait another ${rides.secondRideRelativeTimeHumanized} until ${rides.secondRideAbsoluteTime}.`;
+        }
+        else if (rides.firstRideRelativeTimeInMinutes < 5 && rides.firstRideRelativeTimeInMinutes >= 2) {
+          speechOutput = `Your next tram goes in ${rides.firstRideRelativeTimeHumanized} at ${rides.firstRideAbsoluteTime}.  You better run, GO, GO, GO! Or you can wait ${rides.combinedRelativeTimeHumanized} until ${rides.secondRideAbsoluteTime} for the tram after the next one.`;
+        }
+        else {
+          speechOutput = `Sorry, your next tram goes in ${rides.firstRideRelativeTimeHumanized} at ${rides.firstRideAbsoluteTime}, and you probably will not get it. You should wait ${rides.combinedRelativeTimeHumanized} until ${rides.secondRideAbsoluteTime} for the tram after this one.`;
         }
 
-        const speechOutput = buildSpeechOutputFromResponse(response.body);
         _emitSuccess(this, speechOutput);
       })
       .catch((err) => {
+        console.log('CATCH ERROR: ' + JSON.stringify(err));
         const details = 'Sorry, your webservice call failed! More information: ${JSON.stringify(err)}';
         _emitFailure(this, details);
       });
@@ -57,41 +59,6 @@ function _emitFailure(handlerContext, details) {
   const cardTitle = SKILL_NAME;
   const cardContent = details;
   handlerContext.emit(':tellWithCard', speechOutput, cardTitle, cardContent);
-}
-
-
-function buildSpeechOutputFromResponse(response) {
-  if (response.version !== 2) {
-    return `The Futár webservice returned a response body with a version different than 2: ${response.version}`;
-  }
-
-  if (response.status !== 'OK') {
-    return `The Futár webservice returned a response body with a status different than OK: ${response.status}`;
-  }
-
-  if (response.code !== 200) {
-    return `The Futár webservice returned a response body with a code different than 200: ${response.code}`;
-  }
-
-  const currentTimeInMilliseconds = response.currentTime;
-  const firstRide = response.data.entry.stopTimes[0];
-  const secondRide = response.data.entry.stopTimes[1];
-
-  const futarService = new FutarService();
-  const ride = futarService.getNextRidesInLocalTime(currentTimeInMilliseconds, firstRide, secondRide);
-
-  if (ride.firstRideRelativeTimeInMinutes >= 8) {
-    return `Your next tram goes in ${ride.firstRideRelativeTimeHumanized} at ${ride.firstRideAbsoluteTime}.  You can easily catch it. The second tram goes ${ride.secondRideRelativeTimeHumanized} later at ${ride.secondRideAbsoluteTime}.`;
-  }
-  else if (ride.firstRideRelativeTimeInMinutes < 8 && ride.firstRideRelativeTimeInMinutes >= 5) {
-    return `Your next tram goes in ${ride.firstRideRelativeTimeHumanized} at ${ride.firstRideAbsoluteTime}.  You can still catch it. After that you have to wait another ${ride.secondRideRelativeTimeHumanized} until ${ride.secondRideAbsoluteTime}.`;
-  }
-  else if (ride.firstRideRelativeTimeInMinutes < 5 && ride.firstRideRelativeTimeInMinutes >= 2) {
-    return `Your next tram goes in ${ride.firstRideRelativeTimeHumanized} at ${ride.firstRideAbsoluteTime}.  You better run, GO, GO, GO! Or you can wait ${ride.combinedRelativeTimeHumanized} until ${ride.secondRideAbsoluteTime} for the tram after the next one.`;
-  }
-  else {
-    return `Sorry, your next tram goes in ${ride.firstRideRelativeTimeHumanized} at ${ride.firstRideAbsoluteTime}, and you probably will not get it. You should wait ${ride.combinedRelativeTimeHumanized} until ${ride.secondRideAbsoluteTime} for the tram after this one.`;
-  }
 }
 
 exports.handler = (event, context) => {
