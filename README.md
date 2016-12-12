9b72b3c8263958eb71cf81f5c09adb25add3065a
György Balássy <balassy@users.noreply.github.com>
2016-12-12 06:35:26 +0100

    ADDED: Gulp tasks to create the ZIP package.

--- README.md
+++ README.md
@@ -3,6 +3,15 @@
 An AWS Lambda function and Amazon Alexa skill that allows querying Budapest public transport information.
 
 
+## Development
+
+Useful Gulp tasks:
+
+`gulp pack` - Rebuilds the output ZIP package that contains the code of the Lambda function, and can be uploaded to AWS.
+
+`gulp pack:incremental` - Recompiles the TypeScript files and rebuilds the output ZIP package, using the previously downloaded Node modules in the output folder.  
+
+
 ## About the author
 
 This project is maintaned by [György Balássy](http://gyorgybalassy.wordpress.com).
---
+++ direct/lambda-src/gulpfile.js
@@ -0,0 +1,49 @@
+const gulp = require('gulp');
+const del = require('del');
+const install = require('gulp-install');
+const runSequence = require('run-sequence');
+const ts = require('gulp-typescript');
+const zip = require('gulp-zip');
+
+gulp.task('clean', (done) => 
+  runSequence(['clean:dist'], ['clean:package'], done)
+);
+
+gulp.task('clean:dist', () => 
+  del(['./dist'])
+);
+
+gulp.task('clean:package', () => 
+  del(['./dist.zip'])
+);
+
+gulp.task('tsc', () => {
+  const tsProject = ts.createProject('tsconfig.json');
+  return tsProject.src()
+    .pipe(tsProject())
+    .js
+    .pipe(gulp.dest('./dist'));
+}); 
+
+gulp.task('npm', () => 
+  gulp.src('./package.json')
+    .pipe(gulp.dest('dist/'))
+    .pipe(install({ 
+      production: true,
+      noOptional: true 
+    }))
+);
+
+gulp.task('zip', () =>
+  gulp.src(['dist/**', '!dist/package.json'])
+    .pipe(zip('dist.zip'))
+    .pipe(gulp.dest('./'))
+);
+
+gulp.task('pack', (done) => 
+  runSequence(['clean'], ['tsc', 'npm'], ['zip'], done)
+);
+
+gulp.task('pack:incremental', (done) => 
+  runSequence(['clean:package'], ['tsc'], ['zip'], done)
+);
