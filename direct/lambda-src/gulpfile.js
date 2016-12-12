8ca92d40cd2d8230541a388977ea1d5bbf79a8e3
György Balássy <balassy@users.noreply.github.com>
2016-12-12 09:32:39 +0100

    ADDED: Gulp task to upload to AWS Lambda (incomplete, work in progress).

--- direct/lambda-src/gulpfile.js
+++ direct/lambda-src/gulpfile.js
@@ -1,5 +1,7 @@
+const AWS = require('aws-sdk');
 const gulp = require('gulp');
 const del = require('del');
+const gutil = require('gulp-util');
 const install = require('gulp-install');
 const runSequence = require('run-sequence');
 const ts = require('gulp-typescript');
@@ -39,6 +41,31 @@ gulp.task('zip', () =>
     .pipe(zip('dist.zip'))
     .pipe(gulp.dest('./'))
 );
+
+gulp.task('upload', () => {
+  const FUNCTION_NAME = 'myNextRide'; // TODO
+
+  AWS.config.loadFromPath('./.aws/credentials.json');
+
+  const lambda = new AWS.Lambda();
+  lambda.getFunction({FunctionName: FUNCTION_NAME}, (err, data) => {
+    if (err) {
+      if (err.statusCode === 404) {
+        gutil.log(`Unable to find ${FUNCTION_NAME}!`);
+      }
+      else {
+        gutil.log(`AWS API request failed! ${err}`);
+      }
+
+      return;
+    }
+
+    gutil.log('AWS API call succeeded!');
+
+    // TODO: Incomplete, work in progress.
+  })
+
+});
 
 gulp.task('pack', (done) => 
   runSequence(['clean'], ['tsc', 'npm'], ['zip'], done)
