05ac44136121a6eff5603f0f8c14b182f209b5a5
György Balássy <balassy@users.noreply.github.com>
2016-12-20 22:34:17 +0100

    ADDED: First iteration of tests with multiple states.

--- lambda-src/test/lambda.spec.ts
+++ lambda-src/test/lambda.spec.ts
@@ -1,31 +1,77 @@
 import * as assert from 'assert';
+import { states } from './../src/consts';
 import * as lambda from './../src/index';
-import { RunHelper } from './run-helper';
+import * as RunHelper from './run-helper';
 
 describe('Lambda', () => {
   it('should return a speech string for bus', (done) => {
-    runTest('bus', done);
+    const event1 = RunHelper.buildEvent(
+      'GetNextRideIntent',
+      {
+        Vehicle: {
+          name: 'Vehicle',
+          value: 'bus'
+        }
+      }
+    );
+
+    const context1 = {
+      succeed: (response1: any) => {
+        assert.equal(RunHelper.isSsml(response1), true, 'The service should return a speech text.');
+        assert.equal(RunHelper.isSessionEnded(response1), false, 'The session should not be ended.');
+        assert.equal(RunHelper.isStateSet(response1, states.BUS_MODE), true, 'The next state should be BUS.');
+
+        const event2 = RunHelper.buildEvent(
+          'BusNumberIntent',
+          {
+            BusNumber: {
+              name: 'BusNumber',
+              value: '102'
+            }
+          },
+          states.BUS_MODE);
+
+        const context2 = {
+          succeed: (response2: any) => {
+            assert.equal(RunHelper.isSsml(response2), true, 'The service should return a speech text.');
+            assert.equal(RunHelper.isSessionEnded(response2), true, 'The session should be ended.');
+            assert.equal(RunHelper.isStateSet(response2, states.BUS_MODE), true, 'The next state should be BUS.');
+            done();
+          },
+
+          fail: (err: any) => {
+            done(new Error(err));
+          }
+        };
+
+        lambda.handler(event2, context2);
+      },
+
+      fail: (err: any) => {
+        done(new Error(err));
+      }
+    };
+
+    lambda.handler(event1, context1);
   });
 
   it('should return a speech string for tram', (done) => {
-    runTest('tram', done);
-  });
-
-  function runTest(vehicleName: string, done: Function) {
-    const runHelper = new RunHelper();
-    const intentName = 'GetNextRideIntent';
-    const slots = {
-      Vehicle: {
-        name: 'Vehicle',
-        value: vehicleName
-      }
-    };
-    const event = runHelper.buildEvent(intentName, slots);
+    const event = RunHelper.buildEvent(
+      'GetNextRideIntent',
+      {
+        Vehicle: {
+          name: 'Vehicle',
+          value: 'tram'
+        }
+      });
 
     const context = {
       succeed: (response: any) => {
-        const isSsml = runHelper.isSsml(response);
-        assert.equal(isSsml, true);
+        const isSsml = RunHelper.isSsml(response);
+        assert.equal(isSsml, true, 'The service should return a speech text.');
+
+        const isSessionEnded = RunHelper.isSessionEnded(response);
+        assert.equal(isSessionEnded, true, 'The session should be ended.');
         done();
       },
 
@@ -35,5 +81,5 @@ describe('Lambda', () => {
     };
 
     lambda.handler(event, context);
-  }
+  });
 });
--- lambda-src/test/run-helper.ts
+++ lambda-src/test/run-helper.ts
@@ -1,44 +1,53 @@
+import * as Alexa from 'alexa-sdk';
 import * as uuid from 'uuid';
 const skillConfig = require('./../config/skill.json');  // tslint:disable-line no-require-imports no-var-requires 
 
-export class RunHelper {
+export function buildEvent(intentName: string, slots: any = {}, state?: string) {
+  const requestGuid = uuid();
+  const sessionGuid = uuid();
+  const userGuid = uuid();
+  const now = new Date().toISOString();
 
-  public buildEvent(intentName: string, slots: any = {}) {
-    const requestGuid = uuid();
-    const sessionGuid = uuid();
-    const userGuid = uuid();
-    const now = new Date().toISOString();
-
-    return {
-      session: {
-        sessionId: `SessionId.${sessionGuid}`,
-        application: {
-          applicationId: skillConfig.appId
-        },
-        attributes: {},
-        user: {
-          userId: `amzn1.ask.account.${userGuid}`,
-          accessToken: ''
-        },
-        new: true
+  const result = {
+    session: {
+      sessionId: `SessionId.${sessionGuid}`,
+      application: {
+        applicationId: skillConfig.appId
       },
-      request: {
-        type: 'IntentRequest',
-        requestId: `EdwRequestId.${requestGuid}`,
-        locale: 'en-US',
-        reason: '',
-        timestamp: now,
-        intent: {
-          name: intentName,
-          slots: slots
-        }
+      attributes: {
+        STATE: state
       },
-      version: '1.0'
-    };
-  }
+      user: {
+        userId: `amzn1.ask.account.${userGuid}`,
+        accessToken: ''
+      },
+      new: true
+    },
+    request: {
+      type: 'IntentRequest',
+      requestId: `EdwRequestId.${requestGuid}`,
+      locale: 'en-US',
+      reason: '',
+      timestamp: now,
+      intent: {
+        name: intentName,
+        slots: slots
+      }
+    },
+    version: '1.0'
+  };
 
-  public isSsml(response: any): boolean {
-    return response.response.outputSpeech.type === 'SSML';
-  }
+  return result;
+}
 
+export function isSsml(response: Alexa.ResponseBody): boolean {
+  return response.response.outputSpeech.type === 'SSML';
+}
+
+export function isSessionEnded(response: Alexa.ResponseBody): boolean {
+  return response.response.shouldEndSession;
+}
+
+export function isStateSet(response: Alexa.ResponseBody, expectedState: string): boolean {
+  return response.sessionAttributes.STATE === expectedState;
 }
