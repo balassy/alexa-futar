bab04c78d75691250a15fbd486dcfae19ddc7471
György Balássy <balassy@users.noreply.github.com>
2016-12-21 06:22:27 +0100

    REFACTORED: The RunHelper module is renamed to TestHelper.

--- lambda-src/test/lambda.spec.ts
+++ lambda-src/test/lambda.spec.ts
@@ -2,11 +2,11 @@ import * as Alexa from 'alexa-sdk';
 import * as assert from 'assert';
 import { states } from './../src/consts';
 import * as lambda from './../src/index';
-import * as RunHelper from './run-helper';
+import * as TestHelper from './test-helper';
 
 describe('Lambda', () => {
   it('should return a speech string for bus', async () => {
-    const request1: Alexa.RequestBody = RunHelper.buildRequest(
+    const request1: Alexa.RequestBody = TestHelper.buildRequest(
       'GetNextRideIntent',
       {
         Vehicle: {
@@ -16,13 +16,13 @@ describe('Lambda', () => {
       }
     );
 
-    const response1: Alexa.ResponseBody = await RunHelper.run(lambda, request1);
+    const response1: Alexa.ResponseBody = await TestHelper.run(lambda, request1);
 
-    assert.equal(RunHelper.isSsml(response1), true, 'The service should return a speech text.');
-    assert.equal(RunHelper.isSessionEnded(response1), false, 'The session should not be ended.');
-    assert.equal(RunHelper.isStateSet(response1, states.BUS_MODE), true, 'The next state should be BUS.');
+    assert.equal(TestHelper.isSsml(response1), true, 'The service should return a speech text.');
+    assert.equal(TestHelper.isSessionEnded(response1), false, 'The session should not be ended.');
+    assert.equal(TestHelper.isStateSet(response1, states.BUS_MODE), true, 'The next state should be BUS.');
 
-    const request2: Alexa.RequestBody = RunHelper.buildRequest(
+    const request2: Alexa.RequestBody = TestHelper.buildRequest(
       'BusNumberIntent',
       {
         BusNumber: {
@@ -32,13 +32,13 @@ describe('Lambda', () => {
       },
       states.BUS_MODE);
 
-    const response2: Alexa.ResponseBody = await RunHelper.run(lambda, request2);
-    assert.equal(RunHelper.isSsml(response2), true, 'The service should return a speech text.');
-    assert.equal(RunHelper.isSessionEnded(response2), true, 'The session should be ended.');
+    const response2: Alexa.ResponseBody = await TestHelper.run(lambda, request2);
+    assert.equal(TestHelper.isSsml(response2), true, 'The service should return a speech text.');
+    assert.equal(TestHelper.isSessionEnded(response2), true, 'The session should be ended.');
   });
 
   it('should return a speech string for tram', async () => {
-    const event: Alexa.RequestBody = RunHelper.buildRequest(
+    const event: Alexa.RequestBody = TestHelper.buildRequest(
       'GetNextRideIntent',
       {
         Vehicle: {
@@ -47,8 +47,8 @@ describe('Lambda', () => {
         }
       });
 
-    const response: Alexa.ResponseBody = await RunHelper.run(lambda, event);
-    assert.equal(RunHelper.isSsml(response), true, 'The service should return a speech text.');
-    assert.equal(RunHelper.isSessionEnded(response), true, 'The session should be ended.');
+    const response: Alexa.ResponseBody = await TestHelper.run(lambda, event);
+    assert.equal(TestHelper.isSsml(response), true, 'The service should return a speech text.');
+    assert.equal(TestHelper.isSessionEnded(response), true, 'The session should be ended.');
   });
 });
